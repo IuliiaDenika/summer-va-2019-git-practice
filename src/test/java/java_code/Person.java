12695bb9bac331d6f826cab38acd6351f1c5d7e8
Iuliia Denika <denika4185@gmail.com>
2019-12-05 14:48:51 -0500

    added red color

---
+++ src/test/java/java_code/Person.java
@@ -0,0 +1,7 @@
+public class Person {
+    private int age;
+    public Person (int age){
+        this.age=age;
+
+    }
+}
--- src/test/java/java_code/_SecondClass.java
+++ src/test/java/java_code/_SecondClass.java
@@ -1,3 +1,5 @@
+package java_code;
+
 public class SecondClass {
 
     public static void main(String[] args) {
