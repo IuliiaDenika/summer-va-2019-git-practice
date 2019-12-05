07be39e8cc26fdd952974ed7c996735478b01f2a
Iuliia Denika <denika4185@gmail.com>
2019-12-05 14:27:02 -0500

    added blue color

--- src/test/java/java_code/SecondClass.java
+++ src/test/java/java_code/SecondClass.java
@@ -2,8 +2,9 @@ public class SecondClass {
 
     public static void main(String[] args) {
         String myname= "Iuliia";
-        String myname1= "Denika";
+        String myname1= " Denika";
         System.out.println(myname+ myname1);
+        System.out.println("Iuliia");
 
     }
 }
